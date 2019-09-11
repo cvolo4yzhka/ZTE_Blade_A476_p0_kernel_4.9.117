b6d92d924fda43e357632ffc3c6fe44f8cc8dbb6
xianglai.li <xianglai.li@tcl.com>
2019-09-11 08:45:14 +0300

    initial commit

---
+++ x32_compile_kernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#set architectute
+export ARCH=arm
+#set cross compiller path
+export CROSS_COMPILE=~/Kernel/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+#set directory for build output
+export  KBUILD_OUTPUT=out32
+#set defconfig
+make zte_blade_a476_p0_k49_defconfig
+#start compile
+make zImage-dtb -j16 2>&1 | tee out32/build.log
